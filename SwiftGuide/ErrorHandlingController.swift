a5727803d759b819241cafb1c1023991b2fedcf2
j d <1277492822@qq.com>
2018-11-02 18:54:32 +0800

    Optional Chaining && Error Handling

--- SwiftGuide/ErrorHandlingController.swift
+++ SwiftGuide/ErrorHandlingController.swift
@@ -6,30 +6,176 @@
 //  Copyright © 2018年 ticsmatic. All rights reserved.
 //
 
+// Swift provides first-class support for throwing, catching, propagating, and manipulating recoverable errors at runtime. 抛、捕、转、处理异常
+
+// do-try defer
+
 import UIKit
 
 class ErrorHandlingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
+        
+        // MARK: Representing and Throwing Errors
+        // In Swift, errors are represented by values of types that conform to the Error protocol.
+        enum VendingMachineError: Error {
+            case invalidSelection
+            case insufficientFunds(coinsNeeded: Int)
+            case outOfStock
+        }
+        
+        
+        // MARK: Handling Errors
+        // Propagating Errors Using Throwing Functions
+        struct Item {
+            var price: Int
+            var count: Int
+        }
+        
+        class VendingMachine {
+            var inventory = [
+                "Candy Bar": Item(price: 12, count: 7),
+                "Chips": Item(price: 10, count: 4),
+                "Pretzels": Item(price: 7, count: 11)
+            ]
+            var coinsDeposited = 0
+            
+            func vend(itemNamed name: String) throws {
+                guard let item = inventory[name] else {
+                    // 再抛出去
+                    throw VendingMachineError.invalidSelection
+                }
+                
+                guard item.count > 0 else {
+                    throw VendingMachineError.outOfStock
+                }
+                
+                guard item.price <= coinsDeposited else {
+                    throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+                }
+                
+                coinsDeposited -= item.price
+                
+                var newItem = item
+                newItem.count -= 1
+                inventory[name] = newItem
+                
+                print("Dispensing \(name)")
+            }
+        }
+        
+        let favoriteSnacks = [
+            "Alice": "Chips",
+            "Bob": "Licorice",
+            "Eve": "Pretzels",
+            ]
+        // 这个方法可能会抛出异常，所以调用的时候前面要加try
+        func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+            let snackName = favoriteSnacks[person] ?? "Candy Bar"
+            try vendingMachine.vend(itemNamed: snackName)
+        }
+
+        struct PurchasedSnack {
+            let name: String
+            init(name: String, vendingMachine: VendingMachine) throws {
+                try vendingMachine.vend(itemNamed: name)
+                self.name = name
+            }
+        }
+        
+        
+        // Handling Errors Using Do-Catch 处理
+        /*
+         do {
+            try expression
+            statements
+         } catch pattern 1 {
+            statements
+         } catch pattern 2 where condition {
+            statements
+         } catch {
+            statements
+         }
+         */
+        var vendingMachine = VendingMachine()
+        vendingMachine.coinsDeposited = 8
+        do {
+            try buyFavoriteSnack(person: "Alice", vendingMachine: vendingMachine)
+            print("Success! Yum.")
+        } catch VendingMachineError.invalidSelection {
+            print("Invalid Selection.")
+        } catch VendingMachineError.outOfStock {
+            print("Out of Stock.")
+        } catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+             print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+        } catch {
+            print("Unexpected error: \(error).")
+        }
+        
+        
+        // 内部执行，catch is判断错误先处理一部分
+        func nourish(with item: String) throws {
+            do {
+                try vendingMachine.vend(itemNamed: item)
+            } catch is VendingMachineError {
+                print("Invalid selection, out of stock, or not enough money.")
+            }
+        }
+        do {
+            try nourish(with: "Beet-Flavored Chips")
+        } catch {
+            print("Unexpected non-vending-machine-related error: \(error)")
+        }
+        // Prints "Invalid selection, out of stock, or not enough money."
+        
+        
+        // Converting Errors to Optional Values
+        func someThrowingFunction() throws -> Int {
+            // ...
+            return 1
+        }
+        
+        let x = try? someThrowingFunction()
+        
+        let y: Int?
+        do {
+            y = try someThrowingFunction()
+        } catch {
+            y = nil
+        }
+        print(x, y)
+        
+//        func fetchData() -> Data? {
+//            if let data = try? fetchDataFromDisk() { return data }
+//            if let data = try? fetchDataFromServer() { return data }
+//            return nil
+//        }
+        
+        
+        // Disabling Error Propagation
+        // loads the image resource at a given path or throws an error if the image can’t be loaded
+        // let photo = try! loadImage(atPath: "./Resources/John Appleseed.jpg")
+        
+        
+        
+        // MARK: Specifying Cleanup Actions
+        // Use defer to write a block of code that is executed after all other code in the function, just before the function returns.The code is executed regardless of whether the function throws an error.. You can use defer to write setup and cleanup code next to each other, even though they need to be executed at different times.
+        /*
+        func processFile(filename: String) throws {
+            if exists(filename) {
+                let file = open(filename)
+                defer {
+                    close(file)
+                }
+                while let line = try file.readline() {
+                    // Work with the file.
+                }
+                // close(file) is called here, at the end of the scope.
+            }
+        }
+         */
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- SwiftGuide/OptionalChainingController.swift
+++ SwiftGuide/OptionalChainingController.swift
@@ -6,30 +6,174 @@
 //  Copyright © 2018年 ticsmatic. All rights reserved.
 //
 
+// Optional chaining is a process for querying and calling properties, methods, and subscripts on an optional that might currently be nil. If the optional contains a value, the property, method, or subscript call succeeds;
+// Optional chaining in Swift is similar to messaging nil in Objective-C, but in a way that works for any type, and that can be checked for success or failure.
+
+// 本章节没什么重点，主要是讲类结构体等的属性有些是可选值属性，对这个可选属性访问、赋值
+
 import UIKit
 
 class OptionalChainingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
+        
+        // MARK: Optional Chaining as an Alternative to Forced Unwrapping
+        class Person {
+            var residence: Residence2?
+        }
+        class Residence {
+            var numberOfRooms = 1
+        }
+        let john = Person()
+        
+        // let roomCount = john.residence!.numberOfRooms
+        // this triggers a runtime error
+        
+        if let roomCount = john.residence?.numberOfRooms {
+            print("John's residence has \(roomCount) room(s).")
+        } else {
+            print("Unable to retrieve the number of rooms.")
+        }
+        
+        john.residence = Residence2()
+        
+        
+        
+        // MARK: Defining Model Classes for Optional Chaining
+        class Residence2 {
+            var rooms = [Room]()
+            var numberOfRooms: Int {
+                return rooms.count
+            }
+            subscript(i: Int) -> Room {
+                get {
+                    return rooms[i]
+                }
+                set {
+                    rooms[i] = newValue
+                }
+            }
+            func printNumberOfRooms() {
+                print("The number of rooms is \(numberOfRooms)")
+            }
+            var address: Address?
+        }
+        
+        class Room {
+            let name: String
+            init(name: String) { self.name = name }
+        }
+        
+        class Address {
+            var buildingName: String?
+            var buildingNumber: String?
+            var street: String?
+            func buildingIdentifier() -> String? {
+                if let buildingNumber = buildingNumber, let street = street {
+                    return "\(buildingNumber) \(street)"
+                } else if buildingName != nil {
+                    return buildingName
+                } else {
+                    return nil
+                }
+            }
+        }
+        
+        
+        
+        // MARK: Accessing Properties Through Optional Chaining
+        let john2 = Person()
+        if let roomCount = john2.residence?.numberOfRooms {
+            print("John's residence has \(roomCount) room(s).")
+        } else {
+            print("Unable to retrieve the number of rooms.")
+        }
+        // Prints "Unable to retrieve the number of rooms."
+        
+        let someAddress = Address()
+        someAddress.buildingNumber = "29"
+        someAddress.street = "Acacia Road"
+        john2.residence?.address = someAddress
+        
+        func createAddress() -> Address {
+            print("Function was called.")
+            
+            let someAddress = Address()
+            someAddress.buildingNumber = "29"
+            someAddress.street = "Acacia Road"
+            
+            return someAddress
+        }
+        john2.residence?.address = createAddress()
+        
+        
+        
+        // MARK: Calling Methods Through Optional Chaining
+        // You can use optional chaining to call a method on an optional value, and to check whether that method call is successful. You can do this even if that method does not define a return value.
+        if john2.residence?.printNumberOfRooms() != nil {
+            print("It was possible to print the number of rooms.")
+        } else {
+            print("It was not possible to print the number of rooms.")
+        }
+        // Prints "It was not possible to print the number of rooms."
+        
+        
+        
+        // MARK: Accessing Subscripts Through Optional Chaining
+        if let firstRoomName = john2.residence?[0].name {
+            print("The first room name is \(firstRoomName).")
+        } else {
+            print("Unable to retrieve the first room name.")
+        }
+        // Prints "Unable to retrieve the first room name."
+        
+        // Similarly, you can try to set a new value through a subscript with optional chaining:
+        john2.residence?[0] = Room(name: "Bathroom")
+        
+        let johnsHouse = Residence2()
+        johnsHouse.rooms.append(Room(name: "Living Room"))
+        johnsHouse.rooms.append(Room(name: "Kitchen"))
+        john2.residence = johnsHouse
+        
+        if let firstRoomName = john2.residence?[0].name {
+            print("The first room name is \(firstRoomName).")
+        } else {
+            print("Unable to retrieve the first room name.")
+        }
+        // Prints "The first room name is Living Room."
+
+        
+        // Accessing Subscripts of Optional Type
+        var testScores = ["Dave": [86, 82, 84], "Bev": [79, 94, 81]]
+        testScores["Dave"]?[0] = 91
+        testScores["Bev"]?[0] += 1
+        testScores["Brian"]?[0] = 72
+        // the "Dave" array is now [91, 82, 84] and the "Bev" array is now [80, 94, 81]
+        
+        
+        
+        // MARK: Linking Multiple Levels of Chaining 连接多层可选链式调用
+        if let johnsStreet = john2.residence?.address?.street {
+            print("John's street name is \(johnsStreet).")
+        } else {
+            print("Unable to retrieve the address.")
+        }
+        // Prints "Unable to retrieve the address."
+        
+        let johnsAddress = Address()
+        johnsAddress.buildingName = "The Larches"
+        johnsAddress.street = "Laurel Street"
+        john2.residence?.address = johnsAddress
+        
+        
+        
+        // MARK: Chaining on Methods with Optional Return Values
+        if let buildingIdentifier = john2.residence?.address?.buildingIdentifier() {
+            print("John's building identifier is \(buildingIdentifier).")
+        }
+        // Prints "John's building identifier is The Larches."
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
