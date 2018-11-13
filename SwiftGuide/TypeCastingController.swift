39afc30c9a5ce3359d3d52ca542a1d3356734f4f
j d <1277492822@qq.com>
2018-11-13 19:43:16 +0800

    Type Casting

--- SwiftGuide/TypeCastingController.swift
+++ SwiftGuide/TypeCastingController.swift
@@ -6,30 +6,126 @@
 //  Copyright © 2018年 ticsmatic. All rights reserved.
 //
 
+// Type casting is a way to check the type of an instance, or to treat that instance as a different superclass or subclass from somewhere else in its own class hierarchy.
+
+// Type casting in Swift is implemented with the is and as operators. These two operators provide a simple and expressive way to check the type of a value or cast a value to a different type.
+
+// You can also use type casting to check whether a type conforms to a protocol, as described in Checking for Protocol Conformance.
+
+
 import UIKit
 
 class TypeCastingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
-    }
+        
+        // MARK: Defining a Class Hierarchy for Type Casting
+        class MediaItem {
+            var name: String
+            init(name: String) {
+                self.name = name
+            }
+        }
+        
+        class Movie: MediaItem {
+            var director: String
+            init(name: String, director: String) {
+                self.director = director
+                super.init(name: name)
+            }
+        }
+        
+        class Song: MediaItem {
+            var artist: String
+            init(name: String, artist: String) {
+                self.artist = artist
+                super.init(name: name)
+            }
+        }
+        
+        let library = [
+            Movie(name: "Casablanca", director: "Michael Curtiz"),
+            Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+            Movie(name: "Citizen Kane", director: "Orson Welles"),
+            Song(name: "The One And Only", artist: "Chesney Hawkes"),
+            Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
+        ]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+        
+        
+        // MARK: Checking Type
+        // Use the type check operator (is) to check whether an instance is of a certain subclass type. The type check operator returns true if the instance is of that subclass type and false if it is not.
+        var movieCount = 0
+        var songCount = 0
+        for item in library {
+            if item is Movie {
+                movieCount += 1
+            } else if item is Song {
+                songCount += 1
+            }
+        }
+        print("Media library contains \(movieCount) movies and \(songCount) songs")
+        // Prints "Media library contains 2 movies and 3 songs"
+        
 
-    /*
-    // MARK: - Navigation
+        
+        // MARK: Downcasting
+        // A constant or variable of a certain class type may actually refer to an instance of a subclass behind the scenes. Where you believe this is the case, you can try to downcast to the subclass type with a type cast operator (as? or as!).
+        for item in library {
+            if let movie = item as? Movie {
+                print("Movie: \(movie.name), dir. \(movie.director)")
+            } else if let song = item as? Song {
+                print("Song: \(song.name), dir. \(song.artist)")
+            }
+        }
+        
+        
+        
+        // MARK: Type Casting for Any and AnyObject
+        // Swift provides two special types for working with nonspecific types:
+        
+        // Any can represent an instance of any type at all, including function types.
+        // AnyObject can represent an instance of any class type.
+        var things = [Any]()
+        
+        things.append(0)
+        things.append(0.0)
+        things.append(42)
+        things.append(3.14159)
+        things.append("hello")
+        things.append((3.0, 5.0))
+        things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
+        things.append({ (name: String) -> String in "Hello, \(name)" })
+        things.append({ (name: String) -> String in "hello \(name)" })
+        
+        for thing in things {
+            switch thing {
+            case 0 as Int:
+                print("zero as an Int")
+            case 0 as Double:
+                print("zero as a Double")
+            case let someInt as Int:
+                print("an integer value of \(someInt)")
+            case let someDouble as Double where someDouble > 0:
+                print("a positive double value of \(someDouble)")
+            case is Double:
+                print("some other double value that I don't want to print")
+            case let someString as String:
+                print("a string value of \"\(someString)\"")
+            case let (x, y) as (Double, Double):
+                print("an (x, y) point at \(x), \(y)")
+            case let movie as Movie:
+                print("a movie called \(movie.name), dir. \(movie.director)")
+            case let stringConverter as (String) -> String:
+                print(stringConverter("Michael"))
+            default:
+                print("something else")
+            }
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
